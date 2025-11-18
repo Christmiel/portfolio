f96420c9c22b4aed9e2a48225c862221fa6d827a
Iréné Lokossou <167453336+developer3iwaju@users.noreply.github.com>
2025-11-18 21:22:19 +0100

    stylisation du portolio

--- mon-portfolio/src/app/home/home.component.ts
+++ mon-portfolio/src/app/home/home.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+
+
+    scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
